554d7ebb5eb759ab848ad96ea3fa69e0c56db1e1
Yash Gupta <gyash0440@gmail.com>
2024-07-02 13:38:25 +0530

    fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -5,6 +5,10 @@ import { scrapeAmazonProduct } from "@/lib/scraper";
 import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { NextResponse } from "next/server";
 
+export const maxDuration = 300;
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
@@ -35,7 +39,7 @@ export async function GET() {
               // Update Products in DB
               const updatedProduct = await Product.findOneAndUpdate(
                 {
-                  url: scrapedProduct.url,
+                  url: product.url,
                 },
                 product,
                 {upsert : true ,  new : true}
